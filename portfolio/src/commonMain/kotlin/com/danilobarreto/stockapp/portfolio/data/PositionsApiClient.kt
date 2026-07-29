6b0c8599e5887906e2418a582b08c0d937c80f5c
Danilo Barreto <dgbarreto@gmail.com>
2026-07-29 19:19:51 -0300

    fix: fixes some ref left behind by the add position removal

--- portfolio/src/commonMain/kotlin/com/danilobarreto/stockapp/portfolio/data/PositionsApiClient.kt
+++ portfolio/src/commonMain/kotlin/com/danilobarreto/stockapp/portfolio/data/PositionsApiClient.kt
@@ -1,20 +1,10 @@
 package com.danilobarreto.stockapp.portfolio.data
 
-import com.danilobarreto.stockapp.portfolio.data.dto.CreatePositionRequestDto
-import com.danilobarreto.stockapp.portfolio.data.dto.ErrorResponseDto
 import com.danilobarreto.stockapp.portfolio.data.dto.PortfolioSummaryDto
 import com.danilobarreto.stockapp.portfolio.data.dto.PositionDto
-import com.danilobarreto.stockapp.portfolio.data.dto.UpdatePositionRequestDto
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
-import io.ktor.client.plugins.ClientRequestException
-import io.ktor.client.request.delete
 import io.ktor.client.request.get
-import io.ktor.client.request.patch
-import io.ktor.client.request.post
-import io.ktor.client.request.setBody
-import io.ktor.http.ContentType
-import io.ktor.http.contentType
 
 class PositionsApiClient(
     private val httpClient: HttpClient,
